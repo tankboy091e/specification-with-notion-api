92ea300d1b470b3253879dfaf507bd89f968df6e
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-06-13 15:32:25 +0900

    Fix env

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ module.exports = {
   env: {
     ORIGIN: process.env.ORIGIN,
     NOTION_ORIGIN: process.env.NOTION_ORIGIN,
-    CLIENT_ID: process.env.CLIENT_ID,
     MEMBER_LIST: process.env.MEMBER_LIST,
   },
 }
